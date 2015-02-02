c98a37a49c4f7dcd0a016ef0781664fc7ebf5668
Eldon Nelson <eldon_nelson@ieee.org>
2015-02-01 23:23:41 -0600

    automatic merging and running of cadence coverage runs

--- svsolution/compile_cadence.sh
+++ svsolution/compile_cadence.sh
@@ -1,3 +1,39 @@
+rm -rf cov_work
+
+irun \
+    -clean \
+    -access rw \
+    -coverage all \
+    -uvmhome /asiclibs/libraries/uvm/uvm-1.1d \
+    -quiet \
+    +define+UVM_OBJECT_MUST_HAVE_CONSTRUCTOR \
+    uniq_pkg.sv \
+    emb_pkg.sv \
+    dut_if.sv \
+    dut.sv \
+    TB2_pkg.sv \
+    TB2.sv \
+    -svseed random \
+    +UVM_VERBOSITY=UVM_DEBUG \
+    +UVM_TESTNAME=TB2_test
+
+irun \
+    -clean \
+    -access rw \
+    -coverage all \
+    -uvmhome /asiclibs/libraries/uvm/uvm-1.1d \
+    -quiet \
+    +define+UVM_OBJECT_MUST_HAVE_CONSTRUCTOR \
+    uniq_pkg.sv \
+    emb_pkg.sv \
+    dut_if.sv \
+    dut.sv \
+    TB1_pkg.sv \
+    TB1.sv \
+    -svseed random \
+    +UVM_VERBOSITY=UVM_DEBUG \
+    +UVM_TESTNAME=TB1_test
+
 irun \
     -clean \
     -access rw \
@@ -14,6 +50,10 @@ irun \
     -svseed random \
     +UVM_VERBOSITY=UVM_DEBUG \
     +UVM_TESTNAME=TB0_test
+
+# imc -batch
+imc -exec imc_merge.tcl
+imc -execcmd "load ./cov_work/scope/m1"
 
 # SV TB
 # -covtest TB0 \
---
+++ svsolution/imc_merge.tcl
@@ -0,0 +1,2 @@
+merge ./cov_work/scope/test_sv* -initial_model union_all -out m1 -overwrite -metrics covergroup
+load ./cov_work/scope/m1
